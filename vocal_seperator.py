68aa85c3d74e46e5ac9c665286eada88384dc36f
smthemex <138738845+smthemex@users.noreply.github.com>
2025-08-18 08:24:48 +0800

    Update vocal_seperator.py

--- vocal_seperator.py
+++ vocal_seperator.py
@@ -1,13 +1,14 @@
-import argparse
+# import argparse
 import os
 import shutil
 import subprocess
-from audio_separator.separator import Separator
+
 
 
 
 
 def separator_audio(audio_file_path, audio_separator_model_file,cache_dir,audio_file_prefix):
+    from audio_separator.separator import Separator
     #cache_dir = os.path.join(os.path.dirname(audio_file_path), "vocals")
     os.makedirs(cache_dir, exist_ok=True)
     audio_separator = Separator(
@@ -16,7 +17,7 @@ def separator_audio(audio_file_path, audio_separator_model_file,cache_dir,audio_
         model_file_dir=os.path.dirname(audio_separator_model_file),
     )
     audio_separator.load_model(os.path.basename(audio_separator_model_file))
-    assert audio_separator.model_instance is not None, "Fail to load audio separate model."
+    # assert audio_separator.model_instance is not None, "Fail to load audio separate model."
     outputs = audio_separator.separate(audio_file_path)
     subfolder_path = os.path.dirname(audio_file_path)
     vocal_audio_file = os.path.join(audio_separator.output_dir, outputs[0])
